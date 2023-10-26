c6b3f0146225a8942abd33c7979c23e7f5f04ff6
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2023-10-26 23:55:36 +0530

    Create Method_for_displaying_messages.js

---
+++ Method_for_displaying_messages.js
@@ -0,0 +1,4 @@
+alert("Hello World");//First Method for displaying Message via Alert 
+confirm("Hello World");//Second Method for displaying Message . This dialog will contain Ook and Cancel as alert on Webpage. You can peform separate  operation when the user will click on ok and cancel according to requirement 
+console.log("Hello World");//Third Method.The console.log() method outputs a message to the web console.
+document.write("<h1>Hello World</h1>");//Fourth Method to display message on that particular webpage.
